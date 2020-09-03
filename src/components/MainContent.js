9607c1a02e0bdb17440dde669f2298b061a38eac
Ivan Derlich <e@ivanderlich.com>
2020-09-02 21:10:59 -0300

    Lesson

---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function MainContent() {
+  return (
+    <main>
+      <nav>
+          <h1>Hello a third time!</h1>
+          <ul>
+              <li>Thing 1</li>
+              <li>Thing 2</li>
+              <li>Thing 3</li>
+          </ul>
+      </nav>
+      <p>This is where most of my content will go...</p>
+    </main>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import './index.css';
-import './ex1'
-import './ex2'
-import './ex3'
+// import './ex1'
+// import './ex2'
+// import './ex3'
+import App from './App'
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
+
+

cad4e48f2c8954464f6fc8f7f37c6d9567930acd
Ivan Derlich <e@ivanderlich.com>
2020-09-04 01:20:50 -0300

    More practice

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,38 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import ToDoItem from './ToDoItem'
+import Joke from './Joke'
+import jokesData from './jokesData'
 
 const MainContent = () => {
 
+  const jokesComponents = jokesData.map(joke => <Joke 
+    question={joke.question}
+    punchLine={joke.punchLine}
+    key={joke.id}
+  />)   
+  
   return (
-    <div className="contacts">
-            <div className="contact-card">
-                <img src="http://placekitten.com/300/200"/>
-                <h3>Mr. Whiskerson</h3>
-                <p>Phone: (212) 555-1234</p>
-                <p>Email: mr.whiskaz@catnap.meow</p>
-            </div>
-            
-            <div className="contact-card">
-                <img src="http://placekitten.com/400/200"/>
-                <h3>Fluffykins</h3>
-                <p>Phone: (212) 555-2345</p>
-                <p>Email: fluff@me.com</p>
-            </div>
-            
-            <div className="contact-card">
-                <img src="http://placekitten.com/400/300"/>
-                <h3>Destroyer</h3>
-                <p>Phone: (212) 555-3456</p>
-                <p>Email: ofworlds@yahoo.com</p>
-            </div>
-            
-            <div className="contact-card">
-                <img src="http://placekitten.com/200/100"/>
-                <h3>Felix</h3>
-                <p>Phone: (212) 555-4567</p>
-                <p>Email: thecat@hotmail.com</p>
-            </div>
-        </div>   
+    <div className="contacts">      
+      {jokesComponents}      
+    </div>   
   )
 }
 export default  MainContent;

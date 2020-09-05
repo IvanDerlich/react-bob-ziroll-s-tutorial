34cf3d59ee162f6b11dc3b5bae85f54e78e460e4
Ivan Derlich <e@ivanderlich.com>
2020-09-05 01:28:54 -0300

    More practice

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,20 +1,49 @@
-import React from 'react'
+/*
 
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
 
-// export default function MainContent() {
+*/
 
-//   return (
-//     <div className="todo-list"> 
-//     </div>
-//   )
-// }
-
-export default class App extends React.Component{
+import React, {Component} from 'react'
+  
+export default class App extends Component{
   render(){
     return (
       <div>
-        Code goes here
+        <Header username="Peter"/>
+        <Greeting />
       </div>
+    )
+  }
+}
+
+class Header extends Component{
+  render(){
+    return(
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    )
+  }
+}
+
+class Greeting extends Component{
+  render(){
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+      timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+    return (
+      <h1>Good {timeOfDay} to you, sir or madam!</h1>
     )
   }
 }

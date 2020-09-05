437f80903d9267a2d12800a3d2e408f9d59a6321
Ivan Derlich <e@ivanderlich.com>
2020-09-05 20:34:17 -0300

    React State

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,49 +1,37 @@
-/*
-
-Challenge:
-1. Convert all 3 components to be class-based
-2. Fix the small bug
-
-*/
-
-import React, {Component} from 'react'
-  
-export default class App extends Component{
-  render(){
-    return (
-      <div>
-        <Header username="Peter"/>
-        <Greeting />
-      </div>
-    )
-  }
-}
-
-class Header extends Component{
-  render(){
-    return(
-      <header>
-        <p>Welcome, {this.props.username}!</p>
-      </header>
-    )
-  }
-}
-
-class Greeting extends Component{
-  render(){
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    
-    if (hours < 12) {
-      timeOfDay = "morning"
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon"
-    } else {
-      timeOfDay = "night"
+import React from 'react'
+ 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
     }
-    return (
-      <h1>Good {timeOfDay} to you, sir or madam!</h1>
-    )
+    this.handleOnClick = this.handleOnClick.bind(this)
   }
+  
+  handleOnClick(){         
+    this.setState( prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  render(){
+    
+    return(   
+      <div>
+        <div>
+          <h1>{this.state.count}</h1>
+          <button            
+            onClick={this.handleOnClick}
+          >
+            Change!
+          </button>
+        </div>
+      </div> 
+    )
+  }    
 }
+
+export default App

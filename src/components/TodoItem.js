5174919dedc4810da04983acb1d6a1abd947e1ae
Ivan Derlich <e@ivanderlich.com>
2020-09-07 14:15:56 -0300

    Passing functinos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,25 +1,18 @@
 import React from "react"
 
 export default class TodoItem extends React.Component {
-    constructor(props){
-        super(props) 
-        this.state = {
-            done: props.item.completed
-        }
-    }
 
     render(){
         return (
             <div className="todo-item">
                 <input 
                     type="checkbox" 
-                    checked={this.state.done} 
+                    checked={this.props.item.completed} 
                     //onClick={this.handleClick}
-                    onChange={this.props.onChange}
+                    onChange={() => this.props.handleTodoItemOnChange(this.props.item.id)}
                 />
                 <p>{this.props.item.text}</p>
             </div>
         )
     }
-    
 }

450e4d4c7e4ada5ea4b9f773b884df494548b88f
Ivan Derlich <e@ivanderlich.com>
2020-09-07 21:16:11 -0300

    Conditional Render

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,37 +1,67 @@
 import React from 'react'
- 
+import randomcolor from 'randomcolor'
+
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      count: 0
+      count: 0,
+      color: randomcolor()
     }
-    this.handleOnClick = this.handleOnClick.bind(this)
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
   }
   
-  handleOnClick(){         
-    this.setState( prevState => {
-      return {
-        count: prevState.count + 1
-      }
-    })
-  }
-
   render(){
-    
+    console.log("Render")
     return(   
       <div>
         <div>
-          <h1>{this.state.count}</h1>
+          <h1 style={{color: this.state.color}}>{this.state.count}</h1>
           <button            
-            onClick={this.handleOnClick}
+            onClick={this.increment}
           >
-            Change!
+            Increment!
+          </button>
+          <button            
+            onClick={this.decrement}
+          >
+            Decrement!
           </button>
         </div>
       </div> 
     )
   }    
+
+  componentDidMount(){
+    console.log("Mount")
+  }  
+
+  componentDidUpdate(prevProps, prevState){
+    console.log("Update")    
+    if(prevState.count !== this.state.count){
+        this.setState( prevState => {
+            return{
+                color: randomcolor()
+            }
+        })
+    }    
+  }
+
+  increment(){         
+    this.setState( prevState => {
+      return {
+        count: prevState.count + 1        
+      }
+    })
+  }
+  decrement(){         
+    this.setState( prevState => {
+      return {
+        count: prevState.count - 1        
+      }
+    })
+  }
 }
 
 export default App
